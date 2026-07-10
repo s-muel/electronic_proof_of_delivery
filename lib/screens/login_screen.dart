3263036d2048694e6e5cd2c7bb64d9d1178405a6
s-muel <samuelessuman8@gmail.com>
2026-07-10 00:20:27 +0000

    edited user view UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -263,7 +263,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                   const SizedBox(height: 14),
                   const Text(
-                    'v 0.1.9',
+                    'v 0.2.0',
                     style: TextStyle(
                       color: Colors.black38,
                       fontSize: 10,
